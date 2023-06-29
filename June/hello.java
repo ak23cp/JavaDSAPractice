11afb8cad733c6031105307538d337514c447e9e
amitkumar82001 <amit8kumar2001@gmail.com>
2023-06-30 01:50:10 +0530

    commit2

--- June/hello.java
+++ June/hello.java
@@ -1,5 +1,6 @@
 class hello{
     public static void main(String[] args) {
         System.out.println("hello, how are you ..?");
+        System.out.println("This is first java program");
     }
 }
